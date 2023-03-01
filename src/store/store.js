aa2ad9a2db2978fa5349bcff7c8718c0fa607979
Zhanna Doronina <doronina.jane@mail.ru>
2023-03-01 18:34:35 +0000

    add redux-persist to persist user data - cart state

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import  { compose, applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
 const middleWares = applyMiddleware(logger);
 
-export const store = createStore(rootReducer, undefined, middleWares)
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, middleWares)
+
+export const persistor = persistStore(store);
